fbb657633e842feb4b0582fa47f75e0262e883fb
akbar <143259024+Abdullohn1@users.noreply.github.com>
2024-03-28 18:48:21 +0500

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+function textrename1() {
+  document.querySelector('.work_text').style = "top:0px";
+  document.querySelector('.work_button').style = "display:none";
+}
+function textrename2() {
+    document.querySelector('.work_text').style = "display:none";
+    document.querySelector('.work_button').style = "display:flex";
+}
